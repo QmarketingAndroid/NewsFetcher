113d496471af2a60a769f062c01d6464ba8980cd
nikita.panchenko <nikitapnch@gmail.com>
2021-12-05 12:21:47 +0300

    init

---
+++ app/src/main/java/com/example/newsfetcher/feature/mainscreen/Contract.kt
@@ -0,0 +1,17 @@
+package com.example.newsfetcher.feature.mainscreen
+
+import com.example.newsfetcher.base.Event
+import com.example.newsfetcher.feature.domain.ArticleModel
+
+data class ViewState(
+    val articles: List<ArticleModel>
+)
+
+sealed class UiEvent : Event {
+    data class OnArticleClicked(val index: Int) : UiEvent()
+}
+
+sealed class DataEvent : Event {
+    object LoadArticles : DataEvent()
+    data class OnLoadArticlesSucceed(val articles: List<ArticleModel>) : DataEvent()
+}
